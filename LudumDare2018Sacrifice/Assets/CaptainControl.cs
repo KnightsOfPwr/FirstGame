5c8a3e4fede3ab19eae6cd40df385070fd264906
Erdem <soulofshamans@gmail.com>
2018-12-04 01:59:34 +0100

    Update CaptainControl.cs

--- LudumDare2018Sacrifice/Assets/CaptainControl.cs
+++ LudumDare2018Sacrifice/Assets/CaptainControl.cs
@@ -18,6 +18,7 @@ public class CaptainControl : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
     }
